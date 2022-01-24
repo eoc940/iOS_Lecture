f33dece924bc8282357c1d1331090aedcaa4a7b4
eoc940 <hgk0000@naver.com>
2022-01-24 10:29:10 +0900

    feat: transform operator 학습

--- RxSwift/HelloRxSwift/HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ RxSwift/HelloRxSwift/HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -5,6 +5,48 @@ import PlaygroundSupport
 
 let strikes = PublishSubject<String>()
 let disposeBag = DisposeBag()
+
+struct Student {
+    var score: BehaviorRelay<Int>
+}
+
+let john = Student(score: BehaviorRelay(value: 75))
+let mary = Student(score: BehaviorRelay(value: 95))
+
+let student = PublishSubject<Student>()
+
+student.asObservable()
+    .flatMapLatest {$0.score.asObservable()}
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+student.onNext(john)
+john.score.accept(100)
+
+student.onNext(mary)
+mary.score.accept(80)
+
+john.score.accept(43)
+
+
+/* map
+Observable.of(1,2,3,4,5)
+    .map {
+        return $0 * 2
+    }.subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+ */
+ 
+/* toArray
+Observable.of(1,2,3,4,5)
+    .toArray()
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+ */
+
 /* // ignoreElement
 strikes
     .ignoreElements()
